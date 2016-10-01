b1e52784377c0f1a7a1e3fff20aa5a490ddf0965
Ducros Alix <ducrosalix@hotmail.fr>
2016-10-02 01:09:59 +0200

    Added NetworkInputManager able to receive UDP datagram

---
+++ Disastroids/Assets/Scripts/NetworkInputManager.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Threading;
+using System;
+
+public class NetworkInputManager : MonoBehaviour {
+
+    UDPPacketIO packetIO = null;
+
+    Thread ReadThread = null;
+    bool ReaderRunning = false;
+
+    public int ListeningPort, SendingPort;
+
+    //DEBUG
+    public static float X = 0f;
+
+    void Start () {
+        
+        packetIO = GetComponent<UDPPacketIO>();
+        packetIO.init("127.0.0.1", ListeningPort, SendingPort);
+
+        ReadThread = new Thread(Read);
+        ReaderRunning = true;
+        ReadThread.IsBackground = true;
+        ReadThread.Start();
+    }
+
+    void Destroy()
+    {
+        if(ReaderRunning)
+        {
+            Cancel();
+        }
+    }
+
+    private void Cancel()
+    {
+        if (ReaderRunning)
+        {
+            ReaderRunning = false;
+            ReadThread.Abort();
+        }
+        if (packetIO != null && packetIO.IsOpen())
+        {
+            packetIO.Close();
+            packetIO = null;
+        }
+    }
+
+    private void Read()
+    {
+        try
+        {
+            while (ReaderRunning)
+            {
+                byte[] buffer = new byte[1000];
+                int length = packetIO.ReceivePacket(buffer);
+                //Debug.Log("received packed of len=" + length);
+                if (length > 0)
+                {
+                    //DEBUG : If a UDP datagram is received, let's move the ship to the right indefinetly
+                    Debug.Log("New Message");
+                    Debug.Log(buffer);
+                    X = 0.01f;
+                }
+                else
+                    Thread.Sleep(20);
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.Log("ThreadAbortException"+e);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Disastroids/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+    /// <summary>
+    /// 1 - The speed of the ship
+    /// </summary>
+    public Vector2 speed = new Vector2(50, 50);
+
+    // 2 - Store the movement and the component
+    private Vector2 movement;
+    private Rigidbody2D rigidbodyComponent;
+
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // 3 - Retrieve axis information
+        //float inputX = Input.GetAxis("Horizontal");
+        float inputX = NetworkInputManager.X;
+        float inputY = Input.GetAxis("Vertical");
+
+        // 4 - Movement per direction
+        movement = new Vector2(
+          speed.x * inputX,
+          speed.y * inputY);
+    }
+
+    void FixedUpdate()
+    {
+        // 5 - Get the component and store the reference
+        if (rigidbodyComponent == null) rigidbodyComponent = GetComponent<Rigidbody2D>();
+
+        // 6 - Move the game object
+        rigidbodyComponent.velocity = movement;
+    }
+}
